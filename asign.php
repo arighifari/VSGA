863fc351b4f5763311f19df0b1c926f081c15797
unknown <ghifariakbar46@gmail.com>
2019-10-16 16:44:58 +0700

    PHP SIANG

--- asign.php
+++ asign.php
@@ -7,15 +7,40 @@
     <title>Document</title>
 </head>
 <body>
+<img src="" alt="">
     <?php
-        $bil1 = 14;
+        $bil1 = "3";
         $bil2 = 8;
+        $kata = "Selamat Siang";
 
-        $hasil = $bil1 += $bil2;
-        echo $hasil;
+        for ($i=1; $i <= 5; $i++) { 
+            if ($i%2 == 0) {
+                echo $kata." Genap"."<br>";
+            }
+            else{
+                echo $kata." Ganjil"."<br>";
+            }
+        }
+        for ($i=1; $i <= 5; $i++) { 
+            $kata .= $i;
+        }
+
+        echo $kata."<br>";
+        $replace = str_replace("Siang","Pagi",$kata );
+        echo $replace;
+
+        $kata2 = "Selamat Siang Kelas VSGA";
+        $pecah = explode(" ",$kata2);
         echo "<br>";
-        $hasil = $bil1 += $bil2;
-        echo $hasil;
+        echo $pecah[3];
+        echo "<br>";
+        $sambung = implode(" ",$pecah);
+        echo $sambung;
+        echo "<br>";
+
+        for ($i=count($pecah)-1; $i >= 0; $i--) { 
+            echo $pecah[$i]." ";
+        }
     ?>
 </body>
 </html>
